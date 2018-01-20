46d53586542b85ec1f1b170098e1014faec5a314
wusixing@pku.edu.cn <wusixing@pku.edu.cn>
2018-01-20 09:10:00 +0000

    emb

---
+++ NETEMB-Copy1.py
@@ -0,0 +1,23 @@
+from Multiclassifier_exp import MultiClassificationGAN
+from config import Config
+from Data_Util import DataUtil
+
+data = DataUtil('/ldev/wsx/tmp/netemb/github/dataset/generated_data/eca_blogCatalog3.txt.labeled.reindex' )
+config = Config()
+config.x_dim = data.num_vertex
+config.input_dim = data.num_vertex
+config.num_class = data.num_class
+config.checkpoint_path = 'netemb_eca_blogCatalog3/'
+gan = MultiClassificationGAN(config)
+gan.init_session()
+
+for i in range(0,50000):
+    X, Y = data.next_batch(config.batch_size)
+    res = gan.train_step( X_data=X, Y_data=Y)
+    if i % 500 == 0:
+        if i >0:
+            gan.save_to_checkpoint()
+        samples, labels = gan.figure_step(Y)
+        print(labels)
+        print(gan.test_step(X_data=X,Y_data=Y))
+        print(res)
