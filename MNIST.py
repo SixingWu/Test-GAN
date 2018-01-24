318df6dd0936f4a85c48bb0763782b9951a95c59
mebiuw <wusixing@pku.edu.cn>
2018-01-24 10:44:34 +0800

    bug fixed

--- MNIST.py
+++ MNIST.py
@@ -2,6 +2,8 @@ from Multiclassifier_exp import MultiClassificationGAN
 from tensorflow.examples.tutorials.mnist import input_data
 from config import Config
 import time
+import numpy as np
+from numpy import random
 mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 import matplotlib.pyplot as plt
 import matplotlib.gridspec as gridspec
@@ -26,27 +28,31 @@ gan.init_session()
 current_time = time.time()
 for i in range(0,50000):
     X, Y = mnist.train.next_batch(gan.batch_size)
-    res = gan.train_step( X_data=X, Y_data=Y)
-    if i % 2000 == 0:
+    Y_mask = random.random_integers(0, 1, np.shape(Y)) + 0.0
+    Y_mask -= Y
+    Y_mask = np.maximum(0,Y_mask)
+    YS = abs(Y_mask)
+    YS = YS / np.sum(YS)
+    res = gan.train_step( X_data=X, Y_data=Y, YS_data=YS)
+    if i % 100 == 0:
         print(time.time() - current_time)
         current_time = time.time()
-        if i >0:
-            gan.save_to_checkpoint()
+
         samples, labels = gan.figure_step(Y)
         print(labels)
         print(gan.test_step(X_data=X,Y_data=Y))
-        try:
-            fig = plot(samples)
-            # plt.savefig('out/{}.png'.format(str(i).zfill(3)), bbox_inches='tight')
-            i += 1
-            plt.show()
-            plt.close(fig)
-        except Exception as e:
-            print(e)
-        finally:
-            try:
-                plt.close(fig)
-            except:
-                pass
+        # try:
+        #     fig = plot(samples)
+        #     # plt.savefig('out/{}.png'.format(str(i).zfill(3)), bbox_inches='tight')
+        #     i += 1
+        #     plt.show()
+        #     plt.close(fig)
+        # except Exception as e:
+        #     print(e)
+        # finally:
+        #     try:
+        #         plt.close(fig)
+        #     except:
+        #         pass
 
         print(res)
--- config.py
+++ config.py
@@ -8,7 +8,7 @@ class Config:
     # input
     num_class = 10
     input_dim = 28 * 28
-    batch_size= 100
+    batch_size= 16
     embed=False
     x_dim=input_dim
 
