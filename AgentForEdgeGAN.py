c991bf7ea16140b2e20a004f61a11f08ced41033
mebiuw <wusixing@pku.edu.cn>
2018-04-24 21:33:08 +0800

    Faster

--- AgentForEdgeGAN.py
+++ AgentForEdgeGAN.py
@@ -10,6 +10,9 @@ import os
 
 
 
+
+
+
 def main(argv):
 
     debug = False
@@ -38,6 +41,7 @@ def main(argv):
     gan.init_session()
 
 
+
     current_time = time.time()
 
     for epoch in range(config.epochs):
@@ -64,6 +68,23 @@ def main(argv):
                     data.generate_negative_set(negative_tuple_nums)
             if i % config.internal_test_per_steps == 0:
                     X, Y, h, t, ih, it = data.next_batch(config.batch_size, 'test')
+
+                    def do_infer(config, X_data):
+                        num_class = config.num_class
+                        MX = []
+                        MY = []
+                        for x in X_data:
+                            for i in range(num_class):
+                                MX.append(x)
+                                y = np.zeros([num_class])
+                                y[i] = 1.0
+                                MY.append(y)
+
+                        probs = gan.infer_step(MX, MY)
+                        probs = np.reshape(probs, [-1, num_class])
+                        lables = np.argmax(probs, axis=-1)
+                        return probs, lables
+                    
                     probs,answers = do_infer(config,X)
                     print("Testing:#########")
                     base_scores = 0
