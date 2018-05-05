96dc96016154b3299857a794cbff114053d7100e
wusixing@pku.edu.cn <wusixing@pku.edu.cn>
2018-05-05 14:35:59 +0800

    infering with acc

--- debug.sh
+++ debug.sh
@@ -1,3 +1,4 @@
+export CUDA_VISIBLE_DEVICES=0
 mkdir model/YoutubeDebug
 python3 AgentForEdgeGAN.py /ldev/wsx/tmp/netemb/github/dataset/generated_data/eco_youtube2.txt.labeled.reindex YoutubeDebug
 python3 Predictor.py /ldev/wsx/tmp/netemb/github/dataset/generated_data/eco_youtube2.txt.labeled.reindex YoutubeDebug res/youtubeDebug.txt
